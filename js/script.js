f87cb6bcd0f0e18e5ef41cb360d49cf9ff5b82f2
Rylee Muniu <ryleemuniu3@gmail.com>
2018-08-26 22:54:28 +0300

    script.js altered

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var score=0;
+var total=0;
+var dice_number = 0;
+
+while (true) {
+
+
+var numRolled= Die.Next(1,6);
+
+if (numRolled>1)
+(
+  //pass to other player
+
+
+function pig(var score, var total, var dice_number)
+{
+  // while(dice_number>1)
+  // {
+  //     Random Die = new Random();
+  //     var dice_number = Die.Next(1,6);
+  //     score = score +
+  // }
+  // return pig()
+ total = score+numRolled;
+ //document.write(total);
+
+
+
+
+
+}
+function Roll()
+{
+  document,write(total);
+}
